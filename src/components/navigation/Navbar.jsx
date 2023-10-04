d858d75f5d179441433d73ae04eb7923d11ec2df
Tony-viewz <anthonyosuji32@gmail.com>
2023-10-04 23:45:24 +0100

    update

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBars } from "react-icons/fa";
+import Sidebar from "./Sidebar";
+
 const Navbar = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <nav className=" bg-[#D1E8E2]">
-      <div className=" max-w-[1200px] mx-auto py-4 px-10 flex justify-between items-center">
-        <img src="./download.png" alt="logo" className=" w-20" />
-        <div className="hidden md:flex items-center gap-5 ">
-          <p className="text-[#003366] hover:text-[#FFA500]">Home</p>
-          <p className="text-[#003366]">Product</p>
-          <p className="text-[#003366]">Contact</p>
+    <div>
+      <nav className="bg-[#D1E8E2] relative">
+        <div className="max-w-[1200px] mx-auto py-4 px-10 flex justify-between items-center">
+          <img src="./download.png" alt="logo" className="w-20" />
+          <div className="hidden md:flex items-center gap-5">
+            <p className="text-[#003366] hover:text-[#FFA500]">Home</p>
+            <p className="text-[#003366]">Product</p>
+            <p className="text-[#003366]">Contact</p>
+            <p className="text-[#003366]">About Us</p>
+          </div>
+          <FaBars
+            className="md:hidden cursor-pointer"
+            onClick={toggleSidebar}
+          />
         </div>
-        <FaBars className="md:hidden" />
-      </div>
-    </nav>
+      </nav>
+      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+    </div>
   );
 };
 
--- src/components/navigation/Sidebar.jsx
+++ src/components/navigation/Sidebar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+const Sidebar = ({ isOpen, toggleSidebar }) => {
+  return (
+    <div
+      className={`md:hidden absolute top-16 right-0 z-50 transform transition-transform duration-300 ${
+        isOpen ? "translate-x-0" : "translate-x-full"
+      }`}
+    >
+      <div className="bg-transparent text-black p-4">
+        <p className="text-[#003366] hover:text-[#FFA500] mb-2 cursor-pointer">Home</p>
+        <p className="text-[#003366] hover:text-[#FFA500] mb-2 cursor-pointer">Product</p>
+        <p className="text-[#003366] hover:text-[#FFA500] mb-2 cursor-pointer">Contact</p>
+        <p className="text-[#003366] hover:text-[#FFA500] mb-2 cursor-pointer">About Us</p>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
